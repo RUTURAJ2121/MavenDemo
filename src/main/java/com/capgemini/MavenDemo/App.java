f0643377bbe4f87c93c37226d93fc08035459803
RUTURAJ2121 <ruturajmankar@gmail.com>
2021-02-26 10:34:06 +0530

    First Upload Of MavenDemo

---
+++ src/main/java/com/capgemini/MavenDemo/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.MavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
